2398a307bc0d7f8ef292ac68b5c416c55bde45b1
WTA <satyaveer@wtastudios.com>
2017-11-10 16:24:14 +0530

    intial commit
    onboarding

--- app/src/main/java/com/example/wta/IVS/MainActivity.java
+++ app/src/main/java/com/example/wta/IVS/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.wta.IVS;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText email, password;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.signup);
+        setContentView(R.layout.login);
     }
+
+
 }
