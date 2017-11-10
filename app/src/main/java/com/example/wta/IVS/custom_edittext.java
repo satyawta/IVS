f25ffdb2896f348415330bfbfa87d2df621fd5e0
WTA <satyaveer@wtastudios.com>
2017-11-10 11:36:17 +0530

    intial commit

---
+++ app/src/main/java/com/example/wta/IVS/custom_edittext.java
@@ -0,0 +1,25 @@
+package com.example.wta.IVS;
+
+import android.content.Context;
+import android.support.v7.widget.AppCompatEditText;
+import android.util.AttributeSet;
+
+/**
+ * Created by WTA
+ * 10/11/2017.
+ */
+
+public class custom_edittext extends AppCompatEditText {
+    public custom_edittext(Context context) {
+        super(context);
+    }
+
+    public custom_edittext(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public custom_edittext(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
