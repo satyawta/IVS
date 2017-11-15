fbed6ef3531e70e478c8bea9f6c05116c2a41b4a
WTA <satyaveer@wtastudios.com>
2017-11-15 12:54:01 +0530

    intial commit
                   onboarding
    
                   17 screens done
    
                   2 screens added with all images

--- app/src/main/java/com/example/wta/IVS/MainActivity.java
+++ app/src/main/java/com/example/wta/IVS/MainActivity.java
@@ -1,19 +1,20 @@
 package com.example.wta.IVS;
 
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.teacher_profile_course);
-    }
+        setContentView(R.layout.payment_teacher_course);
 
+    }
 
 }
