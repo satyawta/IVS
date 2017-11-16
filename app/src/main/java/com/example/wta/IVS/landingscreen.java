32acaeb679c06b1fe3ae60341c18cbbc3f32f7fe
WTA <satyaveer@wtastudios.com>
2017-11-16 19:11:15 +0530

    intial commit
                   onboarding
    
                   17 screens done
    
                   2 screens added with all images
    
                   static linkup done

--- app/src/main/java/com/example/wta/IVS/landingscreen.java
+++ app/src/main/java/com/example/wta/IVS/landingscreen.java
@@ -1,13 +1,19 @@
 package com.example.wta.IVS;
 
 import android.app.FragmentManager;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+
 public class landingscreen extends MainActivity {
-    ImageView imageView;
+    ImageView imageView, img;
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +24,11 @@ public class landingscreen extends MainActivity {
         final languagefragment l = new languagefragment();
         imageView = findViewById(R.id.courseimage_1);
 
+        Glide.with(mContext).load(imageView)
+                .thumbnail(0.5f)
+                .crossFade()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(imageView);
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
